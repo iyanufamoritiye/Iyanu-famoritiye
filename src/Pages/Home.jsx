140432191728c8f3470ae199af383f2953f859ff
Iyanu Henry famoritiye <henry.famoritiye@gmail.com>
2024-09-28 01:19:39 +0100

    my first comitt

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeSection1 from "../components/home/homeSection1";
+import HomeSection1 from "../components/home/HomeSection1";
 import HomeSection2 from "../components/home/HomeSection2";
 
 const Home = () => {
